f9f142a120560cecf3234f3b51359f32490cd02a
KyleA <arbourkyle@gmail.com>
2022-02-12 16:13:36 -0500

    wrote script for the first 2 pop ups, and accompanying styles and html changes

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,8 @@ let veg = document.getElementById('vegetables');
 let vegInput = document.getElementById('vegetable');
 let vegNum =  document.getElementById('numberVeg');
 
+let proteinOption = document.getElementById('proteinOption');
+
 let submitBtn = document.getElementById('submitBtn');
 
 let base = document.querySelector('#dishBase');
@@ -15,25 +17,18 @@ let firstPage = document.querySelector('#firstPage');
 var isRice = false;
 var isPasta = false;
 var isPotato = false;
-var vegChoice = "";
-var vegNumButton = 0;
+var vegList;
 
 
 function passTheFirstPage() {
     firstPage.hidden = true;
     veg.hidden = false;
-    passTheVegetables();
 }
 
-function passTheSecondPage() {
-
-    base.hidden = true;
-    veg.hidden = false;
-}
 
 
 //choose rice, pasta, or potato dish
-document.addEventListener('click', function baseOption(event) {
+function baseOption(event) {
     if(event.target == `${rice}`) {
         isRice = true;
         passTheFirstPage();
@@ -45,23 +40,68 @@ document.addEventListener('click', function baseOption(event) {
       else {
           isPotato = true;
           passTheFirstPage();
-      }
-    //   console.log(event.target);
-}); //end of choose base
+        }
+        console.log(event.target);
+    } //end of choose base
+    firstPage.addEventListener('click', baseOption, true);
 
-function passTheVegetables() {
-
-    submitBtn.addEventListener('click', function(event){
     
+//vegetable options
+submitBtn.addEventListener('click', function(event){
     event.preventDefault();
     console.log(event)
-
-    vegChoiceArr = JSON.parse(localStorage.getItem("vegetable")) || [];
+    
+    //make an array in local storage
+    vegChoiceArr = JSON.parse(localStorage.getItem('vegetable')) || [];
     let tempArr = vegInput.value;
     vegChoiceArr.push(tempArr);
-    })//end of listener
+    localStorage.setItem('vegetable', JSON.stringify(vegChoiceArr));
     
-}//end of passTheVegetables
+    // localStorage.clear();
+    if(vegChoiceArr.length === 1) {
+        vegNum.innerText = 1;
+        vegInput.placeholder = '2nd vegetable...';
+    }
+    else if (vegChoiceArr.length === 2) {
+        vegNum.innerText = 2;
+        vegInput.placeholder = 'One more..'
+    }
+    else {
+        vegNum.innerText = 3;
+        // vegInput.placeholder = '1st vegetable';
+        passTheSecondPage();
+        localStorage.clear()
+        veg.hidden = true;
+        proteinOption.hidden = false;
+    }
+    
+    //clears submit field
+    function submit_form() {
+        document.form1.reset(); 
+    }
+    submit_form();
+})//end of listener
+
+
+function passTheSecondPage() {
+   let vegList = JSON.stringify(localStorage.getItem('vegetable'))
+    // console.log(vegList);
+}
+//end of passTheVegetables
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function veganOption() {
     
