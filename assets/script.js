e89ecb6afc94f70fe8217b0202ad56566c9fcb34
KyleA <arbourkyle@gmail.com>
2022-02-11 22:12:33 -0500

    adding functions and testing listeners

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,80 @@
-let firstSpanV = document.getElementById('vegan');
-let firstSpanM = document.getElementById('meat');
-let firstSpanC = document.getElementById('custom');
+let firstSpanV = document.getElementById('rice');
+let firstSpanM = document.getElementById('pasta');
+let firstSpanC = document.getElementById('potato');
 
-let veg = document.getElementById('.vegetables');
+let veg = document.getElementById('vegetables');
+let vegInput = document.getElementById('vegetable');
+let vegNum =  document.getElementById('numberVeg');
 
-let firstPage = document.querySelector('.firstPage');
-console.log(firstPage)
+let submitBtn = document.getElementById('submitBtn');
 
-firstPage.addEventListener('click', function () {
+let base = document.querySelector('#dishBase');
+let firstPage = document.querySelector('#firstPage');
+// console.log(firstPage)
+
+var isRice = false;
+var isPasta = false;
+var isPotato = false;
+var vegChoice = "";
+var vegNumButton = 0;
+
+
+function passTheFirstPage() {
     firstPage.hidden = true;
     veg.hidden = false;
-    vegGroup();
-})
+    passTheVegetables();
+}
 
-function vegGroup(){
+function passTheSecondPage() {
 
-    var multipleCancelButton = new Choices('#choices-multiple-remove-button',{
-    removeItemButton: true,
-    maxItemCount:5,
-    searchResultLimit:5,
-    renderChoiceLimit:5
-});
+    base.hidden = true;
+    veg.hidden = false;
 }
 
 
+//choose rice, pasta, or potato dish
+document.addEventListener('click', function baseOption(event) {
+    if(event.target == `${rice}`) {
+        isRice = true;
+        passTheFirstPage();
+      }
+      else if (event.target == `${pasta}`){
+          isPasta = true;
+          passTheFirstPage();
+      }
+      else {
+          isPotato = true;
+          passTheFirstPage();
+      }
+    //   console.log(event.target);
+}); //end of choose base
+
+function passTheVegetables() {
+
+    submitBtn.addEventListener('click', function(event){
+    
+    event.preventDefault();
+    console.log(event)
+
+    vegChoiceArr = JSON.parse(localStorage.getItem("vegetable")) || [];
+    let tempArr = vegInput.value;
+    vegChoiceArr.push(tempArr);
+    })//end of listener
+    
+}//end of passTheVegetables
+
+function veganOption() {
+    
+}
+
+function meatOption() {
+    
+}
+
+function customOption() {
+    
+}
+function toppingOption() {
+    
+}
 
